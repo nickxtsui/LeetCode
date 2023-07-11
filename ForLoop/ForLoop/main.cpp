5aaacb7e35f7b799844b7fc8eae588f9327e6bba
Xuelin Cui <nickxtsui@gmail.com>
2023-07-10 21:38:44 -0400

    Add files via upload

---
+++ ForLoop/ForLoop/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World" <<endl;
+    // Complete the code.
+    int a,b;
+    cin>>a>>b;
+    
+    
+    for(int i = 8; i < 11; i = i+1)
+    {
+        // make a decision
+        cout<< "i = " << i <<endl;
+    }
+    
+    return 0;
+}
